f1aab04b2c13bcd0dc4867db0ee166a5cd14e229
ArtemKhudiakov <artemka86ber@gmail.com>
2025-11-30 02:54:58 +0500

    Add user module with CRUD operations and validation

---
+++ src/user/dto/update-password.dto.ts
@@ -0,0 +1,12 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class UpdatePasswordDto {
+  @IsString()
+  @IsNotEmpty()
+  oldPassword: string; // previous password
+
+  @IsString()
+  @IsNotEmpty()
+  newPassword: string; // new password
+}
+
---
+++ src/user/user.controller.ts
@@ -0,0 +1,58 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  HttpCode,
+  HttpStatus,
+  BadRequestException,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+import { isValidUUID } from '../common/utils';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    if (!isValidUUID(id)) {
+      throw new BadRequestException('Invalid user ID');
+    }
+    return this.userService.findOne(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updatePasswordDto: UpdatePasswordDto) {
+    if (!isValidUUID(id)) {
+      throw new BadRequestException('Invalid user ID');
+    }
+    return this.userService.update(id, updatePasswordDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    if (!isValidUUID(id)) {
+      throw new BadRequestException('Invalid user ID');
+    }
+    this.userService.remove(id);
+  }
+}
+
