e26c6621fa0b0c8cb8ffc3a7c701ce22aad8623a
ArtemKhudiakov <artemka86ber@gmail.com>
2025-11-30 03:46:55 +0500

    Add artist module with CRUD operations and validation

---
+++ src/artist/artist.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { randomUUID } from 'uuid';
+import { Artist } from './entities/artist.entity';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistDto } from './dto/update-artist.dto';
+
+@Injectable()
+export class ArtistService {
+  private artists: Artist[] = [];
+
+  findAll(): Artist[] {
+    return this.artists;
+  }
+
+  findOne(id: string): Artist {
+    const artist = this.artists.find((a) => a.id === id);
+    if (!artist) {
+      throw new NotFoundException('Artist not found');
+    }
+    return artist;
+  }
+
+  create(createArtistDto: CreateArtistDto): Artist {
+    const newArtist: Artist = {
+      id: randomUUID(),
+      name: createArtistDto.name,
+      grammy: createArtistDto.grammy,
+    };
+
+    this.artists.push(newArtist);
+    return newArtist;
+  }
+
+  update(id: string, updateArtistDto: UpdateArtistDto): Artist {
+    const artist = this.artists.find((a) => a.id === id);
+    if (!artist) {
+      throw new NotFoundException('Artist not found');
+    }
+
+    artist.name = updateArtistDto.name;
+    artist.grammy = updateArtistDto.grammy;
+
+    return artist;
+  }
+
+  remove(id: string): void {
+    const index = this.artists.findIndex((a) => a.id === id);
+    if (index === -1) {
+      throw new NotFoundException('Artist not found');
+    }
+
+    this.artists.splice(index, 1);
+  }
+}
+
